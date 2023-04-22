3a8aab319c24b2fb92060224241cd063874bc7b1
AshishJadhav <vinodjadhav1845@gmail.com>
2023-04-22 23:40:46 +0530

    a

---
+++ lambda.py
@@ -0,0 +1,13 @@
+# Define a lambda function that takes a single argument and returns its square
+square = lambda x: x ** 2
+
+# Call the lambda function with an argument of 5 and print the result
+result = square(5)
+print(result)  # Output: 25
+
+# Define a lambda function that takes two arguments and returns their product
+product = lambda x, y: x * y
+
+# Call the lambda function with arguments 5 and 10 and print the result
+result = product(5, 10)
+print(result)  # Output: 50
